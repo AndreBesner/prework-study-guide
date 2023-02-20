9f95e63eb980393fa2716f8410b82ea321c6d113
Andre Besner <besner.andre@gmail.com>
2023-02-20 14:56:42 -0500

    Added all js code, added html bullet points, changed css colours to look cooler

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,35 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];   //Declares array var with study guide topics
-
-for(var x = 0; x < topics.length; x++){             //Loops through array and prints topic to console
-    console.log(topics[x]);
-}
-
 /*
-if(topic === "HTML"){
-    console.log("Let's study HTML!");
-} else if(topic === "CSS"){
-    console.log("Let's study CSS");
-} else if(topic === "Git"){
-    console.log("Let's study Git");
-} else if(topic === "JavaScript"){
-    console.log("Let's study JavaScript");
-} else {
-    console.log('Please try again');
-}
+Simple JavaScript code to help randomly select a topic to study
+Feb20,23
+Andre Besner
 */
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];                      //Declares array var with study guide topics
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];    //Math function to generate random topic to study
+
+function listTopics(){                                                  //Function to print values of topics array
+    for(var x = 0; x < topics.length; x++){
+        console.log(topics[x]);
+    }
+}
+
+function selectTopic(){                                                 //Function to select a topic
+    if(randomTopic === "HTML"){
+        console.log("Let's study HTML!");
+    } else if(randomTopic === "CSS"){
+        console.log("Let's study CSS!");
+    } else if(randomTopic === "Git"){
+        console.log("Let's study Git!");
+    } else if(randomTopic === "JavaScript"){
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}
+
+console.log("Here are the topics we learned through pre-work!");         //Lists topics
+listTopics();
+
+console.log("What subject should we study first?");                      //Outputs randomly selected topic
+selectTopic();
